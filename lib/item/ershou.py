8e0b8b89390484c1c1bfac69d2e56ac72a038dc7
Simon000 <linadou101@gmail.com>
2020-04-06 16:21:41 +0800

    与增加的community、link配置对齐，调整分隔符为分号
    调整了输出字段的顺序，同时更改了分隔符为分号。原始的记录中用逗号分隔很容易出现分列错乱的情况，因为原始信息中可能带逗号

--- lib/item/ershou.py
+++ lib/item/ershou.py
@@ -6,18 +6,22 @@
 
 
 class ErShou(object):
-    def __init__(self, district, area, name, price, desc, pic):
+    def __init__(self, district, area, community, price, desc, link, name):
         self.district = district
         self.area = area
+        self.community = community
         self.price = price
-        self.name = name
         self.desc = desc
-        self.pic = pic
+        self.link=link
+        self.name = name
+        
+
 
     def text(self):
-        return self.district + "," + \
-                self.area + "," + \
-                self.name + "," + \
-                self.price + "," + \
-                self.desc + "," + \
-                self.pic
+        return self.district + ";" + \
+                self.area + ";" + \
+                self.community + ";" + \
+                self.price + ";" + \
+                self.desc + ";" + \
+                self.link + ";" + \
+                self.name
